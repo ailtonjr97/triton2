7f191da14f4ac04bf91d96082d3f3cf1a2e20ac7
root <root@intranet>
2024-09-10 09:17:10 -0300

    minor changes

--- routes/consultaRoutes.js
+++ routes/consultaRoutes.js
@@ -14,7 +14,7 @@ const {
         consultaDa0010,
         consultaDa1010,
         consultaUniaoDa0Da1,
-        consultaHistlise,
+        //consultaHistlise,
         consultaSb1
     } = require('../controllers/consultaController');
 
@@ -31,7 +31,7 @@ router.get("/consulta-da0010", consultaDa0010);
 router.get("/consulta-da1010", consultaDa1010);
 router.get("/consulta-uniao-da0-da1", consultaUniaoDa0Da1);
 
-router.get("/consulta-histlise", consultaHistlise);
+//router.get("/consulta-histlise", consultaHistlise);
 router.get("/consulta-sb1", consultaSb1);
 
 router.get("/cliente", cliente);
