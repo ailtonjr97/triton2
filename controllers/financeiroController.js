3720d95f54636abee99279cf9e98223ffda449a7
root <root@intranet>
2024-12-13 08:46:21 -0300

    trocado aprovador de analise para o Alex

--- controllers/financeiroController.js
+++ controllers/financeiroController.js
@@ -201,8 +201,8 @@ async function docOk(req, res) {
     try {
         const now = new Date();
 
-        const recipient = req.query.valor <= 20000.00 ? 'aux.adm@fibracem.com' : 'gerente.fin@fibracem.com';
-        const approver = req.query.valor <= 20000.00 ? 'Natali Evelin Peres Pereira' : 'Sthefany Jackeline Kataloski';
+        const recipient = req.query.valor <= 20000.00 ? 'aux.adm@fibracem.com' : 'contabil1@fibracem.com';
+        const approver = req.query.valor <= 20000.00 ? 'Natali Evelin Peres Pereira' : 'Alex Lemes da Silva';
 
         await sendEmailCadastro(
             recipient,
